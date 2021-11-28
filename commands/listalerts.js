de5a666ecefd65fff85a39b6053aead8b335ba52
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-11-28 01:27:06 -0500

    finished every discord command

--- commands/listalerts.js
+++ commands/listalerts.js
@@ -5,6 +5,20 @@ module.exports = {
         .setName('listalerts')
         .setDescription('List all gas tracking alerts'),
     async execute(interaction, db) {
-        await interaction.reply('Here are your active alerts:');
+        const uID = interaction.user.id;
+        console.log(uID);
+        const query = { 'userLists.userID': uID };
+        const trackerList = await db.find(query).toArray();
+
+        let finalString = "You have trackers set at the following GWEI amounts: ";
+
+        trackerList.forEach(level => {
+            console.log(level);
+            finalString = finalString + "**" + level.gasLevel + "**, ";
+        });
+
+        finalString = finalString.substring(0, finalString.length - 2);
+
+        await interaction.reply(finalString + ".");
     },
 };
