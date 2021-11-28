3ad7b895bb1f362f1720aecbb1d68cf82b1e1dba
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2021-11-28 00:32:08 -0500

    finished setting alert

--- commands/setalert.js
+++ commands/setalert.js
@@ -13,6 +13,7 @@ module.exports = {
                 .setDescription('Have this be a repeat alert (once a day)')),
     async execute(interaction, db) {
         const gasLevel = interaction.options.getInteger('gas');
+        const repeatValue = interaction.options.getBoolean('repeat') || false;
 
         // If not acceptable level, return
         if (gasLevel < 0 || gasLevel > 10000) {
@@ -26,28 +27,68 @@ module.exports = {
 
         // If object exists, append userList
         if (gasObj) {
+            // Check if user already in list
             const userInserted = gasObj.userLists.find(user => user.userID === interaction.user.id);
-            console.log(userInserted);
+            if (userInserted) {
+                console.log(`Alert already set, user ${userInserted.userID}`);
+                await interaction.reply(`You already have an alert set at ${gasLevel} GWEI!`);
+                return;
+            }
+            // If not on the list
+            else {
+                // Create new user object
+                const newUser = {
+                    userID: interaction.user.id,
+                    userObj: interaction.user,
+                    guildID: interaction.guild.id,
+                    guildName: interaction.guild.name,
+                    timeInserted: new Date(),
+                    repeat: repeatValue,
+                };
+
+                // Extend the user list
+                const extendedList = [
+                    ...gasObj.userLists,
+                    newUser
+                ];
+
+                // Update document
+                const filter = { gasLevel: gasLevel };
+                const updateDoc = {
+                    $set: {
+                        userLists: extendedList
+                    },
+                };
+
+                const result = await db.updateOne(filter, updateDoc);
+                console.log(
+                    `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+                );
+            }
         }
+        // If object not already created
         else {
+            // Format insert data
             const insertData = {
                 gasLevel: gasLevel,
-                timeInserted: new Date(),
+                timeCreated: new Date(),
                 userLists: [
                     {
                         userID: interaction.user.id,
                         userObj: interaction.user,
                         guildID: interaction.guild.id,
                         guildName: interaction.guild.name,
-                        repeat: false,
+                        timeInserted: new Date(),
+                        repeat: repeatValue,
                     },
                 ],
             }
 
+            // Insert data
             const result = await db.insertOne(insertData);
             console.log(`Created new tracking alert for ${gasLevel} GWEI, for user ${interaction.user.username}, with ID:${result.insertedId}`);
         }
-
+        // Send confirmation message 
         await interaction.reply(`Set alert at ${gasLevel} GWEI!`);
     },
 };
